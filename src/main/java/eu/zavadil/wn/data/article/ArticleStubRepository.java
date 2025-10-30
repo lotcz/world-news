2b8b7f74bbb675df8a341b9402d05e29e6dac909
Karel Zavadil <zavadil@incomaker.com>
2025-10-30 23:54:14 +0100

    fill lastUpdatedOn

--- src/main/java/eu/zavadil/wn/data/article/ArticleStubRepository.java
+++ src/main/java/eu/zavadil/wn/data/article/ArticleStubRepository.java
@@ -6,42 +6,90 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.Instant;
+
 public interface ArticleStubRepository extends EntityRepository<ArticleStub> {
 
 	@Modifying
 	@Transactional
 	@Query("""
 		update ArticleStub a
-		set a.topicId = :topicId
+		set a.topicId = :topicId, a.lastUpdatedOn = :lastUpdatedOn
 		where a.id = :articleId
 		""")
-	void moveToTopic(@Param("articleId") int articleId, @Param("topicId") int topicId);
+	void moveToTopic(
+		@Param("articleId") int articleId,
+		@Param("topicId") int topicId,
+		@Param("lastUpdatedOn") Instant lastUpdatedOn
+	);
+
+	@Modifying
+	@Transactional
+	default void moveToTopic(int articleId, int topicId) {
+		this.moveToTopic(articleId, topicId, Instant.now());
+	}
 
 	@Modifying
 	@Transactional
 	@Query("""
 		update ArticleStub a
-		set a.topicId = :toTopicId
+		set a.topicId = :toTopicId, a.lastUpdatedOn = :lastUpdatedOn
 		where a.topicId = :fromTopicId
 		""")
-	void mergeTopics(@Param("fromTopicId") int fromTopicId, @Param("toTopicId") int toTopicId);
+	void mergeTopics(
+		@Param("fromTopicId") int fromTopicId,
+		@Param("toTopicId") int toTopicId,
+		@Param("lastUpdatedOn") Instant lastUpdatedOn
+	);
+
+	@Modifying
+	@Transactional
+	default void mergeTopics(int fromTopicId, int toTopicId) {
+		this.mergeTopics(fromTopicId, toTopicId, Instant.now());
+	}
 
 	@Modifying
 	@Transactional
 	@Query("""
 		update ArticleStub a
-		set a.articleType = :articleType
+		set a.articleType = :articleType, a.lastUpdatedOn = :lastUpdatedOn
 		where a.id = :articleId
 		""")
-	void changeArticleType(@Param("articleId") int articleId, @Param("articleType") ArticleType articleType);
+	void changeArticleType(
+		@Param("articleId") int articleId,
+		@Param("articleType") ArticleType articleType,
+		@Param("lastUpdatedOn") Instant lastUpdatedOn
+	);
+
+	@Modifying
+	@Transactional
+	default void changeArticleType(int articleId, ArticleType articleType) {
+		this.changeArticleType(articleId, articleType, Instant.now());
+	}
 
 	@Modifying
 	@Transactional
 	@Query("""
 		update ArticleStub a
-		set a.mainImageId = :imageId, a.mainImageIsIllustrative = :illustrative
+		set a.mainImageId = :imageId,
+			a.mainImageIsIllustrative = :illustrative,
+			a.lastUpdatedOn = :lastUpdatedOn
 		where a.id = :articleId
 		""")
-	void changeImage(@Param("articleId") int articleId, @Param("imageId") Integer imageId, @Param("illustrative") boolean illustrative);
+	void changeImage(
+		@Param("articleId") int articleId,
+		@Param("imageId") Integer imageId,
+		@Param("illustrative") boolean illustrative,
+		@Param("lastUpdatedOn") Instant lastUpdatedOn
+	);
 
+	@Modifying
+	@Transactional
+	default void changeImage(
+		int topicId,
+		Integer imageId,
+		boolean illustrative
+	) {
+		this.changeImage(topicId, imageId, illustrative, Instant.now());
+	}
 }
--- src/main/java/eu/zavadil/wn/data/topic/TopicStubRepository.java
+++ src/main/java/eu/zavadil/wn/data/topic/TopicStubRepository.java
@@ -18,7 +18,7 @@ public interface TopicStubRepository extends EntityRepository<TopicStub> {
 		set t.lastUpdatedOn = :lastUpdatedOn
 		where t.id = :topicId
 		""")
-	void markAsChanged(@Param("topicId") int topicId, Instant lastUpdatedOn);
+	void markAsChanged(@Param("topicId") int topicId, @Param("lastUpdatedOn") Instant lastUpdatedOn);
 
 	@Modifying
 	@Transactional
@@ -30,27 +30,71 @@ public interface TopicStubRepository extends EntityRepository<TopicStub> {
 	@Transactional
 	@Query("""
 		update TopicStub t
-		set t.articleType = :articleType
+		set t.articleType = :articleType, t.lastUpdatedOn = :lastUpdatedOn
 		where t.id = :topicId
 		""")
-	void changeArticleType(@Param("topicId") int topicId, @Param("articleType") ArticleType articleType);
+	void changeArticleType(
+		@Param("topicId") int topicId,
+		@Param("articleType") ArticleType articleType,
+		@Param("lastUpdatedOn") Instant lastUpdatedOn
+	);
+
+	@Modifying
+	@Transactional
+	default void changeArticleType(int topicId, ArticleType articleType) {
+		this.changeArticleType(topicId, articleType, Instant.now());
+	}
 
 	@Modifying
 	@Transactional
 	@Query("""
 		update TopicStub t
-		set t.mainImageId = :imageId, t.mainImageIsIllustrative = :illustrative
+		set t.mainImageId = :imageId,
+			t.mainImageIsIllustrative = :illustrative,
+			t.lastUpdatedOn = :lastUpdatedOn
 		where t.id = :topicId
 		""")
-	void changeImage(@Param("topicId") int topicId, @Param("imageId") Integer imageId, @Param("illustrative") boolean illustrative);
+	void changeImage(
+		@Param("topicId") int topicId,
+		@Param("imageId") Integer imageId,
+		@Param("illustrative") boolean illustrative,
+		@Param("lastUpdatedOn") Instant lastUpdatedOn
+	);
+
+	@Modifying
+	@Transactional
+	default void changeImage(
+		int topicId,
+		Integer imageId,
+		boolean illustrative
+	) {
+		this.changeImage(topicId, imageId, illustrative, Instant.now());
+	}
 
 	@Modifying
 	@Transactional
 	@Query("""
 		update TopicStub t
-		set t.mainImageId = :imageId, t.mainImageIsIllustrative = :illustrative
+		set t.mainImageId = :imageId,
+			t.mainImageIsIllustrative = :illustrative,
+			t.lastUpdatedOn = :lastUpdatedOn
 		where t.id = :topicId and t.mainImageId is null
 		""")
-	void changeImageIfEmpty(@Param("topicId") int topicId, @Param("imageId") Integer imageId, @Param("illustrative") boolean illustrative);
+	void changeImageIfEmpty(
+		@Param("topicId") int topicId,
+		@Param("imageId") Integer imageId,
+		@Param("illustrative") boolean illustrative,
+		@Param("lastUpdatedOn") Instant lastUpdatedOn
+	);
+
+	@Modifying
+	@Transactional
+	default void changeImageIfEmpty(
+		int topicId,
+		Integer imageId,
+		boolean illustrative
+	) {
+		this.changeImageIfEmpty(topicId, imageId, illustrative, Instant.now());
+	}
 
 }
